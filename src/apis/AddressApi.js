93a2220ed19afc65d6e7a6e9ce9c73ccd46198d8
= <=>
2021-07-13 10:44:36 +0100

    address book page working

--- src/apis/AddressApi.js
+++ src/apis/AddressApi.js
@@ -2,11 +2,6 @@ import axios from 'axios'
 import BaseUrl from './BaseUrl';
 import setHeader from '../utilities/Header';
 
-// const customHeader = {
-//     headers: {
-//         Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJleHAiOjE2MjY2NTQyNjIsImlhdCI6MTYyNTc5MDI2Mn0._PokXtQcizInYlietSM5JKECB3ai9T0DIVZ3kJJ2HRw`,
-//     }
-// };
 
 const customHeader = setHeader();
 
@@ -14,7 +9,7 @@ const AddressApi = {
 
     getUserDefaultAddress: async () => {
       const { data: defaultAddress } = await axios.get(
-        `${BaseUrl}/api/address/default`, 
+        `${BaseUrl}/api/shipping_address/default`, 
         customHeader
       );
       
@@ -23,32 +18,55 @@ const AddressApi = {
 
     getAllUserAddresses: async () => {
         const { data: addresses } = await axios.get(
-          `${BaseUrl}/api/address/all`, 
+          `${BaseUrl}/api/shipping_address/`, 
           customHeader
         );
         
         return addresses;
       },
 
-
     addAddress :  async (event, formData, setAlertBox) => {
         event.preventDefault();
-        await axios.post( `${BaseUrl}/api/address/add`, formData, customHeader)
-        .then((response) => {
-            console.log("Address Upload Response: ", response);
-                const {status} = response;
-                if (status >= 400 ){
-                    setAlertBox({state:true, message:'Something went wrong!!', type:'error'});
-                } else {
-                    setAlertBox({state:true, message:'Address has been successfully added!', type:'success'});
+        try {
+            await axios.post( `${BaseUrl}/api/shipping_address/add`, formData, customHeader)
+            .then((response) => {
+                    const {status} = response;
+                    if (status === 200){
+                        setAlertBox({state:true, message:'Address has been added successfully!', type:'success'});
+                    }
                 }
+            ); 
+        }catch (e) {
+            const {status}= e.response;
+            if (status === 400 || status === 500){
+                setAlertBox({state:true, message:'Something went wrong!!', type:'error'});
             }
-        );
+        }
+    },
+
+    editAddress :  async (event, formData, setAlertBox) => {
+        event.preventDefault();
+        try{
+            await axios.put( `${BaseUrl}/api/shipping_address/edit/${formData.id}`, formData, customHeader)
+            .then((response) => {
+                    const {status} = response;
+                    console.log(status)
+                    if (status === 200){
+                        setAlertBox({state:true, message:'Address has been updated successfully!', type:'success'});
+                    } 
+                }
+            );
+        }catch (e) {
+            const {status}= e.response;
+            if (status === 400 || status === 500){
+                setAlertBox({state:true, message:'Something went wrong!!', type:'error'});
+            }
+        }
     },
 
     deleteAddress: async (id) => {
         let { data: response } = await axios.delete(
-          `${BaseUrl}/api/address/delete/${id}`,
+          `${BaseUrl}/api/shipping_address/delete/${id}`,
           customHeader
         );
         
