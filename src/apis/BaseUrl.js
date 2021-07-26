db5215dfb23359fd9cc35324e9407b0018bf41f6
Francis Oyiogu <francis.oyiogu@decagon.dev>
2021-07-26 01:00:30 +0100

    fixed merge

--- src/apis/BaseUrl.js
+++ src/apis/BaseUrl.js
@@ -1,5 +1,3 @@
-// const BaseUrl = "https://safariwebstoreapp.herokuapp.com";
-
 const BaseUrl = "http://localhost:8045";
-
+// const BaseUrl = "https://safariwebstoreapp.herokuapp.com";
 export default BaseUrl;
